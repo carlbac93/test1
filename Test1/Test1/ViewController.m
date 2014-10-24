23d227041f3cb280a8955c0306d15558e11cd27e
Carl Bashir <cbashir@servantsystems.com>
2014-10-24 12:08:05 -0400

    CBashir - 1.0.1 - Implemented iss53 halfway

--- Test1/Test1/ViewController.m
+++ Test1/Test1/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //iss53 start
+    //iss53 middle
 }
 
 - (void)didReceiveMemoryWarning {
