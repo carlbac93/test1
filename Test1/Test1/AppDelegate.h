6d9c087ca2f4c3d83d58866657a6a34718ef6969
Carl Bashir <cbashir@servantsystems.com>
2014-10-24 10:57:35 -0400

    CBashir - 1.0.0 - Initial commit

---
+++ Test1/Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by Carl Bachir on 10/24/14.
+//  Copyright (c) 2014 Servant Systems, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
